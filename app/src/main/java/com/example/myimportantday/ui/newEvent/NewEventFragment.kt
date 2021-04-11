8a616fc7ee5107c7e64c6566f809288f4b36c331
Zsolt Kiss <56066989+Zsolti0819@users.noreply.github.com>
2021-04-11 09:55:24 +0200

    Basic bottom navigation bar

---
+++ app/src/main/java/com/example/myimportantday/ui/newEvent/NewEventFragment.kt
@@ -0,0 +1,31 @@
+package com.example.myimportantday.ui.newEvent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.myimportantday.R
+
+class NewEventFragment : Fragment() {
+
+    private lateinit var newEventViewModel: NewEventViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        newEventViewModel =
+                ViewModelProvider(this).get(NewEventViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_new_event, container, false)
+        val textView: TextView = root.findViewById(R.id.text_new_event)
+        newEventViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
