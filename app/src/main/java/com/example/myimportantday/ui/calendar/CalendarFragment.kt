187c7daa16ab47ffff210043f60804113a2bce70
Zsolt Kiss <56066989+Zsolti0819@users.noreply.github.com>
2021-04-17 15:47:12 +0200

    Small GUI update

--- app/src/main/java/com/example/myimportantday/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/myimportantday/ui/calendar/CalendarFragment.kt
@@ -22,10 +22,6 @@ class CalendarFragment : Fragment() {
         calendarViewModel =
                 ViewModelProvider(this).get(CalendarViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_calendar, container, false)
-        val textView: TextView = root.findViewById(R.id.text_calendar)
-        calendarViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
